59889482cee0a9dd3129f8408ca1569e2f0039d8
mardukfi <marduk.fi@educatic.unam.mx>
2020-02-22 13:27:30 -0600

    clase 21 febrero anexo

--- ejercicios/VirusViewer/VirusViewer/ViewController.swift
+++ ejercicios/VirusViewer/VirusViewer/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("virus"){
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
-
-
 }
 
