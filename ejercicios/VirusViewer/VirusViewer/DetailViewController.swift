d2e0826fe5fef8f53d9bb666c037549d9adb1c42
mardukfi <marduk.fi@educatic.unam.mx>
2020-02-28 21:51:46 -0600

    Clase 28 de febrero 2020

---
+++ ejercicios/VirusViewer/VirusViewer/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  VirusViewer
+//
+//  Created by Marduk Perez de Lara on 28/02/20.
+//  Copyright © 2020 Marduk Perez de Lara. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
