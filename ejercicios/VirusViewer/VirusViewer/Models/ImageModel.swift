55b0fecfbec687f073c92eec8ecf02c997bb7497
mardukfi <marduk.fi@educatic.unam.mx>
2020-03-08 01:32:21 -0600

    adding mvc to virusViewer

---
+++ ejercicios/VirusViewer/VirusViewer/Models/ImageModel.swift
@@ -0,0 +1,9 @@
+//
+//  ImageModel.swift
+//  VirusViewer
+//
+//  Created by Marduk Perez de Lara on 08/03/20.
+//  Copyright © 2020 Marduk Perez de Lara. All rights reserved.
+//
+
+import Foundation
