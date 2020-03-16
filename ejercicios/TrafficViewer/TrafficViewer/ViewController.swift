40182206b6c7a877d1b1db6cc328611331e93cc0
mardukfi <marduk.fi@educatic.unam.mx>
2020-03-16 13:12:05 -0600

    clase 13 de marzo

---
+++ ejercicios/TrafficViewer/TrafficViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficViewer
+//
+//  Created by Marduk Perez de Lara on 13/03/20.
+//  Copyright © 2020 Marduk Perez de Lara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
