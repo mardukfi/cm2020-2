f652156f1c3a00b6bb78b368f0d08a0d3a082f46
mardukfi <marduk.fi@educatic.unam.mx>
2020-03-16 13:12:23 -0600

    clase 13 de  marzo

--- ejercicios/TrafficViewer/TrafficViewer/ViewController.swift
+++ ejercicios/TrafficViewer/TrafficViewer/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func unwindToGreen(unwindSegue: UIStoryboardSegue){
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
 
-
+    @IBAction func buttonSegue(_ sender: Any) {
+        
+        if(textField.text == "Hola"){
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
 }
 
